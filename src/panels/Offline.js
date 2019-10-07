ed2559efbbc0a94176f011a69639a983223dca67
Daniil <littlebigcrafting@gmail.com>
2019-10-07 10:30:08 +0300

    Add comments to Offline.js

--- src/panels/Offline.js
+++ src/panels/Offline.js
@@ -9,20 +9,20 @@ export class Offline extends React.Component {
 
     reload = () => {
         if (navigator.onLine) {
-            window.showAlert(false, getMessage('no_internet'));
+            window.showAlert(false, getMessage('no_internet')); //Получаем сообщение об отсутствии интернета
             return;
         }
-        window.location.reload();
+        window.location.reload();   //Перерисовываем окно
     };
 
     render() {
         return (
-            <Panel id="offline" theme="white">
+            <Panel id="offline" theme="white"> //PlaceHolder Оффлайн
                 <Div>
                     <Div>offline</Div>
                     
                     <Button 
-                        onClick={ this.reload }
+                        onClick={ this.reload } //Кнопка перезагрузки
                     >reload</Button>
                 </Div>
             </Panel>
